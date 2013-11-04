51b34781b035c121dae0134bd60eefee31ad980b
collin <collin@ubuntu.ubuntu-domain>
2013-11-03 19:35:40 -0600

    collin commit

---
+++ src/com/example/dyel_net/display_exercises.java
@@ -0,0 +1,39 @@
+package com.example.dyel_net;
+
+import android.view.View;
+import android.widget.CheckBox;
+import android.os.Bundle;
+
+public class display_exercises {
+	
+	void display_exercises_toggle_forearms(View v)
+	{
+		CheckBox temp = (CheckBox)v;
+	}	
+	
+	void display_exercises_toggle_arms()
+	{
+	
+	}
+	
+	void display_exercises_toggle_chest()
+	{
+	
+	}
+	
+	void display_exercises_toggle_back()
+	{
+	
+	}
+	
+	void display_exercises_toggle_legs()
+	{
+	
+	}
+	
+	void display_exercises_toggle_shoulders()
+	{
+		
+	}
+
+}
