bd46cdd42fb705f548ce697f98302d685204d497
gfpastorek <gfpastorek@gmail.com>
2013-11-10 21:45:40 -0600

    fixed errors

--- src/com/example/dyel_net/MainActivity.java
+++ src/com/example/dyel_net/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends Activity {
 	
 	/**************PROCESS CLASSES***************************/
 	public Workout workout;
-	
+	public RoutineView routineView;
 	
 	public void login(View v)
 	{
@@ -195,7 +195,17 @@ public class MainActivity extends Activity {
 	// TODO
 	private void cli_routine_view(TextView TV)
 	{
+		String status = routineView.getStatus();
+ 		
+		if(status == "routines")
+		{
+			cli_routineView_routines(TV);
+		}
+	}
 		
+	private void cli_routineView_routines(TextView TV){
+		LinearLayout L = (LinearLayout)TV.getParent();
+		routineView.viewWeeks();
 	}
 
 
@@ -258,6 +268,13 @@ public class MainActivity extends Activity {
 	{
 		gotoLayout(R.layout.workingout);
 	}
+	
+	public void gotoRoutineView(View v){
+		routineView = new RoutineView(this);
+		gotoLayout(R.layout.routine_view);
+		routineView.viewRoutines();
+	}
+
 	
 	/****************TESTING METHODS*************************/
 	public void connectToDatabase(View v)
