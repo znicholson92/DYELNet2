67772146930e8395f88287d9c57f3ff907784fcf
collin <collin@ubuntu.ubuntu-domain>
2013-12-01 19:49:34 -0600

    .

--- src/com/example/dyel_net/LinReg.java
+++ src/com/example/dyel_net/LinReg.java
@@ -22,7 +22,7 @@ public class LinReg {
 
 	String name_to_ID(String exer_in)
 	{
-		String temp = "SELECT exerciseID FROM exercise WHERE name == " + exer_in + ";";
+		String temp = "SELECT exerciseID FROM exercise WHERE name = '" + exer_in + "';";
 		
 		String jString = app.con.readQuery(temp);
 		String ID = "";
@@ -40,8 +40,8 @@ public class LinReg {
 	@SuppressWarnings("null")
 	private ArrayList<String> pull_days(String exer_id)
 	{
-		String temp = "SELECT dayID FROM session WHERE exerciseID == " + exer_id + "AND username " +
-				"== " + app.con.username() + ";";
+		String temp = "SELECT dayID FROM session WHERE exerciseID = '" + exer_id + "' AND username " +
+				"= '" + app.con.username() + "';";
 		String jString = app.con.readQuery(temp);
 		
 		ArrayList<String> dayIDs = new ArrayList<String>();
@@ -65,7 +65,7 @@ public class LinReg {
 		return "ISH";
 	}
 	
-	ArrayList<DataNode> grab_data(ArrayList<String> dayIDs)
+	ArrayList<DataNode> grab_data(ArrayList<String> dayIDs, String exer_id)
 	{
 		ArrayList<DataNode> nodes = new ArrayList<DataNode>();
 		
@@ -76,8 +76,27 @@ public class LinReg {
 			DataNode temp = new DataNode();
 			temp.dayID = dayIDs.get(iter);
 			
-			String query_for_date = "SELECT datetime FROM session where dayID == " + temp.dayID + ";"; 
+			String query_for_week = "SELECT week FROM schedule_week INNER JOIN schedule_day" +
+					" ON schedule_week.weekID = schedule_day.weekID WHERE dayID = '" + temp.dayID + "';"; 
+			
+			String week_s = "";
+			try {
+				JSONObject jsonObject = new JSONObject(query_for_week);
+				JSONArray jArray = jsonObject.getJSONArray("data");
+				JSONObject j = jArray.getJSONObject(0);
+				week_s = (String) j.get("week");
+			} catch (JSONException e) {e.printStackTrace();}
+			
+			int week = Integer.parseInt(week_s);
+			temp.week = week;
+			
+			ArrayList<String> weights = new ArrayList<String>();
+			
+			String get_max = "SELECT MAX(weight) AS Max, reps FROM _set WHERE dayID = '" + temp.dayID
+					+ "' exerciseID = '" + exer_id + "';";
 
+			
+			
 		}
 		
 		
@@ -92,6 +111,9 @@ public class LinReg {
 		exer_id = app.cache.getExerciseID(exercise);
 		exer_id = name_to_ID(exer_in);
 		dayIDs = pull_days(exer_id);
+		
+		ArrayList<DataNode> nodes = grab_data(dayIDs, exer_id);
+		
 		//have list of day_ids... 
 		
 		
